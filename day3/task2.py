f0745d9ab1e2ddb2efce13f4ab9a1475b0e0bb8c
Dan Skaggs <dan@skaggsfamily.ws>
2022-12-07 00:08:39 -0500

    Day 3 task 2 complete

--- day3/task2.py
+++ day3/task2.py
@@ -1,35 +1,31 @@
 import re
 
-input = open('input.txt', 'r')
-
-count = 0
 priority = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
 priority_sum = 0
+rucksack_contents = []
+index = 0
 
+input = open('input.txt', 'r')
+for line in input:
+    rucksack_contents.append( re.sub('[^a-zA-Z]+', '', line ) )
+input.close()
 
-def identify_compartments( contents ):
-    content_length = int( len( contents ) )
-    a_end = int( content_length / 2 )
-    b_start = int( ( content_length / 2 ) )
-    b_end = content_length
-    a_contents = contents[ 0 : a_end ]
-    b_contents = contents[ b_start : b_end : 1 ]
-
-    return a_contents, b_contents
-
-def identify_duplicate( a, b ):
-    for char in a:
-        if( b.find( char ) > -1 ):
+def find_badge( group ):
+    for char in group[0]:
+        if group[1].find( char ) > -1 and group[2].find( char ) > -1:
             return char
 
-def identify_group()
+def get_priority( badge ):
+    global priority
+    badge_priority = priority.find( badge ) + 1
+    return ( badge_priority )
 
-for line in input:
-    a, b = identify_compartments( re.sub( '![a-zA-Z]','', line ) )
-    duplicate = identify_duplicate( a, b )
-    
-    priority_sum += ( priority.find( duplicate ) + 1 )
 
-input.close()
+while index < len( rucksack_contents ):
+    group = rucksack_contents[ index: index+3: 1 ]    
+    badge = find_badge( group )
+    priority_sum += get_priority( badge )
+    #print( str( start ) + " :: " + str(stop) + " :: " + badge + " :: " + priority[ priority.find( badge )  ] + " :: " + str( get_priority( badge ) ) + " :: " + str( group ) )
+    index +=3
 
 print( priority_sum )
