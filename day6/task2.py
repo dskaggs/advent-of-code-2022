2d7762d83969b8aeea49e04cbc909b7fddc7823b
Dan Skaggs <dan@skaggsfamily.ws>
2022-12-09 20:08:41 -0500

    Day 6 Task 2 complete

---
+++ day6/task2.py
@@ -0,0 +1,25 @@
+with open("input.txt") as f:
+    input = f.read().strip()
+    f.close()
+
+block_start = 0  #start index of 4-character block to check
+block_end = 14    #end index of 4-character block to check
+
+def has_duplicates( s ):
+    has_duplicates = False
+
+    for l in s:
+        if s.count(l) > 1:
+            has_duplicates = True
+    
+    return has_duplicates
+
+while block_end < len(input):
+    s = input[block_start:block_end]
+
+    if( has_duplicates(s) == False ):
+        print( block_end )
+        break
+    else:
+        block_start +=1
+        block_end +=1
